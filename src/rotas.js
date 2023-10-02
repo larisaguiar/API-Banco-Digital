5106cb2891294ee7e0a91bfa6fca208cc5fd2a42
laris <larissaolaguiar@gamil.com>
2023-10-02 16:34:09 -0300

    organizando arquivo

--- src/rotas.js
+++ src/rotas.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const {listarContas, listarContaId, criarConta}= require('./controladores/contas')
+
+const { listarContas, criarConta, atualizarConta, deletarConta, exibirSaldo, exibirExtrato} = require('./controladores/contas');
+
+const { depositar, sacar, transferir } = require('./controladores/transacoes');
 
 const rotas =  express();
 
-rotas.get('/contas',listarContas);
+rotas.get('/contas', listarContas);
 
-rotas.get('/contas/:id', listarContaId);
+rotas.post('/contas', criarConta);
 
-rotas.post('/contas', criarConta)
+rotas.put('/contas/:numeroConta/usuario', atualizarConta);
+
+rotas.delete('/contas/:numeroConta', deletarConta);
+
+rotas.get('/contas/saldo', exibirSaldo);
+
+rotas.get('/contas/extrato', exibirExtrato);
+
+rotas.post('/transacoes/depositar', depositar);
+
+rotas.post('/transacoes/sacar', sacar);
+
+rotas.post('/transacoes/transferir', transferir);
 
 module.exports = rotas;
 
